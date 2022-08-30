2baca6932d47d7b03456656c76902435470780da
David Igheose <47044294+Davidohis@users.noreply.github.com>
2022-08-30 12:26:10 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import Theme from "../styles/theme";
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -6,6 +7,17 @@ export default function App({ Component, pageProps }) {
       <Theme>
         <Component {...pageProps} />
       </Theme>
+
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-PJHQ9G4XKG"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PJHQ9G4XKG');
+</script>
     </>
   );
 }
