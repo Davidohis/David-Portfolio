d723f3d873e4d216e58721401608b134f09ca80d
David Igheose <47044294+Davidohis@users.noreply.github.com>
2022-08-30 12:30:21 +0100

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,4 @@
 import Theme from "../styles/theme";
-import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
@@ -7,15 +6,6 @@ export default function App({ Component, pageProps }) {
       <Theme>
         <Component {...pageProps} />
       </Theme>
-
-<Script async src="https://www.googletagmanager.com/gtag/js?id=G-PJHQ9G4XKG"></Script>
-<Script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-PJHQ9G4XKG');
-</Script>
     </>
   );
 }
